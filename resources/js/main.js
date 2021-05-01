4a6f35e072324243f962074749717090e376a162
Ahmed Khaled <eng.ahmedkhalid50@gmail.com>
2021-05-01 23:22:51 +0200

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,16 @@
+// var slider = document.getElementById('slide');
+// var slideImg = ['slide1.jpg', 'slide2.jpg', 'slide3.jpg', 'slide5.jpg', 'slide6.jpg', 'slide7.jpg'];
+
+// setInterval(Slide, 4000);
+// var index = 0;
+
+// function Slide() {
+
+//     if (index < slideImg.length) {
+//         console.log(index)
+//         slider.style.backgroundImage = "url(resources/images/slider/" + slideImg[index] + ")";
+//         index++;
+//     } else {
+//         index = 0;
+//     }
+// }
